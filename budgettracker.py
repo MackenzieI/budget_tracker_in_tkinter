f833a2c7a596b889261eac08580a9a45beea9b76
Mackenzie <84205474+MackenzieI@users.noreply.github.com>
2025-08-12 14:27:40 -0500

    Added labels and stylings

--- budgettracker.py
+++ budgettracker.py
@@ -1,18 +1,37 @@
+import tkinter as tk
 from tkinter import *
 import pandas as pd 
-from tkhtmlview import HTMLLabel
 
 class BudgetTracker: 
     def __init__(self, root):
         self.root = root
         self.root.title("Budget App")
 
+        self.my_label = tk.Label(root, text="Budget Tracker", font=("Helvetica", 14, "bold"), justify=tk.CENTER)
+        self.my_label.grid(row=0,column=0, columnspan=2, padx=15,pady=15)
+
+        self.date_label = tk.Label(root, text="Date (MM-DD-YYYY): ", font=("Helvetica", 12), justify=tk.LEFT)
+        self.date_label.grid(row=1, column=0)
+        self.date_entry = tk.Entry(root)
+        self.date_entry.grid(row=1, column=1, padx=10, pady=5)
+
+        self.description_label = tk.Label(root, text="Description: ", font=("Helvetica", 12))
+        self.description_label.grid(row=2,column=0)
+        self.description_entry = tk.Entry(root)
+        self.description_entry.grid(row=2,column=1, padx=10, pady=5)        
+        
+        self.amount_label = tk.Label(root, text="Amount: ", font=("Helvetica", 12))
+        self.amount_label.grid(row=3,column=0)
+        self.amount_entry = tk.Entry(root)
+        self.amount_entry.grid(row=3,column=1, padx=10, pady=10)        
+        
+        self.type_label = tk.Label(root, text="Income or expense: ", font=("Helvetica", 12))
+        self.type_label.grid(row=4,column=0)
+        self.type_entry = tk.Entry(root)
+        self.type_entry.grid(row=4,column=1, padx=10, pady=10)
+
 root = Tk()
 app = BudgetTracker(root)
-my_label = HTMLLabel(root, html="""
-        <h4>Budget Tracker</h4>
-    """)
-my_label.pack(pady=20, padx=20)
 root.mainloop()
 
 try:
