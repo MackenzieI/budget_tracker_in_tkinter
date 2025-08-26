94a8cc53d9d823e7be70455471de822bdf5fbef2
Mackenzie <84205474+MackenzieI@users.noreply.github.com>
2025-08-25 22:42:30 -0500

    Add pages; button needs to be fixed
    Added
    -Page class
    -A budget page and a debt page
    -A main page that should switch between them
    Need to do next
    -Button is not changing the current page

--- budgettracker.py
+++ budgettracker.py
@@ -1,59 +1,94 @@
 import tkinter as tk
 from tkinter import *
-import pandas as pd 
+import pandas as pd
 
-root = Tk()
-budget_frame = tk.Frame(root) 
-budget_frame.grid(row=0,column=0)
+class Page(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
 
-class BudgetTracker: 
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Budget App")
+    def show(self):
+        self.lift()
 
-        self.my_label = tk.Label(budget_frame, text="Budget Tracker", font=("Helvetica", 14, "bold"), justify=tk.CENTER)
-        self.my_label.grid(row=0,column=0, columnspan=2, padx=15,pady=15)
+class BudgetPage(Page):
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
 
-        self.date_label = tk.Label(budget_frame, text="Date (MM-DD-YYYY): ", font=("Helvetica", 12), justify=tk.LEFT)
-        self.date_label.grid(row=1, column=0)
-        self.date_entry = tk.Entry(budget_frame)
+        root.wm_geometry("300x300")
+
+        tk.Label(self, text="Budget Tracker", font=("Helvetica", 14, "bold")).grid(row=0, column=0, columnspan=2, padx=15, pady=15)
+
+        tk.Label(self, text="Date (MM-DD-YYYY): ", font=("Helvetica", 12)).grid(row=1, column=0)
+        self.date_entry = tk.Entry(self)
         self.date_entry.grid(row=1, column=1, padx=10, pady=5)
 
-        self.description_label = tk.Label(budget_frame, text="Description: ", font=("Helvetica", 12))
-        self.description_label.grid(row=2,column=0)
-        self.description_entry = tk.Entry(budget_frame)
-        self.description_entry.grid(row=2,column=1, padx=10, pady=5)        
-        
-        self.amount_label = tk.Label(budget_frame, text="Amount: ", font=("Helvetica", 12))
-        self.amount_label.grid(row=3,column=0)
-        self.amount_entry = tk.Entry(budget_frame)
-        self.amount_entry.grid(row=3,column=1, padx=10, pady=5)        
-        
-        self.type_label = tk.Label(budget_frame, text="Income or expense: ", font=("Helvetica", 12))
-        self.type_label.grid(row=4,column=0)
-        self.type_entry = tk.Entry(budget_frame)
-        self.type_entry.grid(row=4,column=1, padx=10, pady=5)
+        tk.Label(self, text="Description: ", font=("Helvetica", 12)).grid(row=2, column=0)
+        self.description_entry = tk.Entry(self)
+        self.description_entry.grid(row=2, column=1, padx=10, pady=5)
 
-        self.debtButton = tk.Button(
-            text="Debt",
-            font=("Helvetica", 12),
-            #command=debtPage.show)
+        tk.Label(self, text="Amount: ", font=("Helvetica", 12)).grid(row=3, column=0)
+        self.amount_entry = tk.Entry(self)
+        self.amount_entry.grid(row=3, column=1, padx=10, pady=5)
+
+        tk.Label(self, text="Income or Expense: ", font=("Helvetica", 12)).grid(row=4, column=0)
+        self.type_entry = tk.Entry(self)
+        self.type_entry.grid(row=4, column=1, padx=10, pady=5)
+
+class DebtPage(Page):
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+
+        tk.Label(self, text="Debts", font=("Helvetica", 14, "bold")).grid(row=0, column=0, columnspan=2, padx=15, pady=10)
+
+        tk.Label(self, text="Loan Name: ", font=("Helvetica", 12)).grid(row=1, column=0)
+
+        tk.Label(self, text="Amount: ", font=("Helvetica", 12)).grid(row=2, column=0)
+
+        tk.Label(self, text="Interest Rate (%): ", font=("Helvetica", 12)).grid(row=3, column=0)
+
+# 
+# 
+# DEBT PAGE IS NOT SHOWN WHEN BUTTON IS CLICKED
+#
+#
+
+class MainView(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+
+        self.container = tk.Frame(self)
+        self.container.pack(side="top", fill="both", expand=True)
+
+        self.button_frame = tk.Frame(self)
+        self.button_frame.pack(side="top", fill="x", expand=False)
+
+        self.pages = {}
+        for PageClass in (BudgetPage, DebtPage):
+            page = PageClass(self.container)
+            self.pages[PageClass] = page
+            page.place(x=0, y=0, relwidth=1, relheight=1)
+
+        self.debtButton = tk.Button( 
+            text="Debt", 
+            font=("Helvetica", 12), 
+            command=self.pages[BudgetPage].show
         )
-        self.debtButton.grid(row=5,column=0, padx=10, pady=15)
+        self.debtButton.pack()
 
+        self.pages[BudgetPage].show() 
 
+try: 
+    df = pd.read_csv('./records.csv') 
+except Exception as e: data = { 
+    "Date": [], 
+    "Description": [], 
+    "Amount": [], 
+    "Type": [] 
+    } 
 
-app = BudgetTracker(root)
-root.mainloop()
+df = pd.DataFrame(data)
 
-try:
-    df = pd.read_csv('./records.csv')
-except Exception as e: 
-    data = {
-        "Date": [],
-        "Description": [],
-        "Amount": [],
-        "Type": []
-    }
-    
-    df = pd.DataFrame(data)
+if __name__ == "__main__":
+    root = tk.Tk()
+    main = MainView(root)
+    main.pack(side="top", fill="both", expand=True)
+    root.mainloop()
